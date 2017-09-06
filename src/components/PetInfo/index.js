f421ec3ba8ae2ffcc2c59eb346c8aed4feeb157a
ashek <sheacspear@gmail.com>
2017-09-06 12:23:25 +0300

    remove trash

--- src/components/PetInfo/index.js
+++ src/components/PetInfo/index.js
@@ -2,14 +2,44 @@ import React from 'react';
 import {View, StyleSheet, Text, Image} from 'react-native';
 
 
-
 export default class PetInfo extends React.Component {
-
 
     render() {
         return (
-            <Text>PetInfo</Text>
+            <View style={{flex: 1, flexDirection: 'row',}}>
+                <View style={{flex: 2}}>
+                    <Text>Foto</Text>
+                </View>
+                <View style={{flex: 3}}>
+                    <View style={{flex: 1}}>
+                        <Text>Leopold</Text>
+                    </View>
+                    <View style={{flexDirection: 'row',}}>
+                        <View style={{flex: 1, alignItems: 'flex-start', flexDirection: 'row'}}>
+                            <Text
+                                style={{fontSize: 12, color: 'rgba(96,100,109, 1)'}}>Weight
+                                29 g</Text>
+                            <Image style={styles.icon}
+                                   source={require('./../../assets/icons/ic_add_circle_outline_black_18dp.png')}
+                            />
+                        </View>
+                        <View style={{flex: 2, alignItems: 'flex-end'}}>
+                            <Text
+                                style={{fontSize: 12, color: 'rgba(96,100,109, 1)'}}>Next
+                                feed 10.12.2017 </Text>
+                        </View>
+                    </View>
+
+                </View>
+            </View>
         );
     }
 }
 
+
+const styles = StyleSheet.create({
+    icon: {
+        width: 18,
+        height: 18,
+    }
+});
